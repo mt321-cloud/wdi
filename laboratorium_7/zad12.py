b19d94ae92c1621c0f61bb817c56df4278d1eb22
Your Name Here <your@email.com>
2021-11-19 17:46:16 +0100

    zadanie 12 z laboratorium 7

---
+++ laboratorium_7/zad12.py
@@ -0,0 +1,76 @@
+# Zadanie 12.
+# Wykorzystując listy proszę napisać program, który symuluje wykonywanie działań arytmetycznych w słupku. Program powinien pobierać od użytkownika liczby M i N oraz odpowiedni znak D (znak działania). Należy zaimplementować możliwość dodawania i odejmowania liczb mniejszych niż 10^10 i obsłużyć wyjątki, gdy użytkownik zażąda wykonania mnożenia lub dzielenia.
+m = input("Podaj pierwsza liczbe:")
+n = input("Podaj druga liczbe:")
+d = input("Podaj znak dzialania:")
+
+if int(m) >= 10**10 or int(n) >= 10**10:
+    raise ValueError("Podano zbyt duza liczbe")
+
+if d == '*' or d == '/':
+    raise ValueError("Nieobslugiwana operacja arytmetyczna")
+
+czy_wynik_ujemny = False
+if int(m) < int(n):
+    m, n = n, m
+    czy_wynik_ujemny = True
+
+m_cyfry = [int(c) for c in m]
+n_cyfry = [int(c) for c in n]
+
+r = abs(len(m_cyfry)-len(n_cyfry))
+
+# dopisanie zer z przodu krótszej liczby
+
+if len(m_cyfry) > len(n_cyfry):
+    for x in range(r):
+        n_cyfry.insert(0, 0)
+else:
+    for x in range(r):
+        m_cyfry.insert(0, 0)
+
+#print(m_cyfry, n_cyfry)
+
+wynik_cyfry = []
+
+if d == '+':
+    i = len(m_cyfry)-1
+    czy_nadwyzka = False
+    while i >= 0:
+        suma = m_cyfry[i]+n_cyfry[i]
+        if czy_nadwyzka:
+            suma += 1
+        if suma > 9:
+            suma = suma-10
+            czy_nadwyzka = True
+        else:
+            czy_nadwyzka = False
+        wynik_cyfry.insert(0, suma)
+        i -= 1
+    if czy_nadwyzka:
+        wynik_cyfry.insert(0, 1)
+
+elif d == '-':
+    i = len(m_cyfry)-1
+    czy_pozyczka = False
+    while i >= 0:
+        roznica = m_cyfry[i]-n_cyfry[i]
+        if czy_pozyczka:
+            roznica -= 1
+        if roznica < 0:
+            roznica = roznica+10
+            czy_pozyczka = True
+        else:
+            czy_pozyczka = False
+        wynik_cyfry.insert(0, roznica)
+        i -= 1
+
+if czy_wynik_ujemny:
+    wynik = '-'
+else:
+    wynik = ''
+for cyfra in wynik_cyfry:
+    wynik += str(cyfra)
+wynik = int(wynik)
+
+print('Wynik działania:', wynik)
